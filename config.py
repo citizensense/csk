1d9b9198996453906ca09c50e3a30ce6d8e4c764
Tom Keene <tom@theanthillsocial.co.uk>
2000-01-01 02:06:31 +0000

    Updated alphasense calculations

--- config.py
+++ config.py
@@ -12,94 +12,94 @@ def init():
         '0000000080c169f1':{
             'name':'kit 1',
             'alphasense':{
-                'weSN1':0,        # VpcbWE-SN1-zero 
-                'weSN2':0,        # VpcbWE-SN2-zero
-                'weSN3':0,        # VpcbWE-SN3-zero
-                'aeSN1':0,        # VpcbAE-SN1-zero
-                'aeSN2':0,        # VpcbAE-SN2-zero
-                'aeSN3':0,        # VpcbAE-SN3-zero
-                'SN1sensi':0.0,   # "Sensitivity (mV/ppb)"
-                'SN2sensi':0.0,   # "Sensitivity (mV/ppb)"
-                'SN3sensi':0.0    # "Sensitivity (mV/ppb)" 
+                'we1z':0,         # VpcbWE-SN1-zero 
+                'we2z':0,         # VpcbWE-SN2-zero
+                'we3z':0,         # VpcbWE-SN3-zero
+                'ae1z':0,         # VpcbAE-SN1-zero
+                'ae2z':0,         # VpcbAE-SN2-zero
+                'ae3z':0,         # VpcbAE-SN3-zero
+                '1sensi':0.0,     # "Sensitivity (mV/ppb)"
+                '2sensi':0.0,     # "Sensitivity (mV/ppb)"
+                '3sensi':0.0      # "Sensitivity (mV/ppb)" 
             },
             'methane':False
         },
         '000000006217b4aa':{
             'name':'kit 2',
             'alphasense':{
-                'weSN1':300,        # VpcbWE-SN1-zero 
-                'weSN2':409,        # VpcbWE-SN2-zero
-                'weSN3':281,        # VpcbWE-SN3-zero
-                'aeSN1':303,        # VpcbAE-SN1-zero
-                'aeSN2':419,        # VpcbAE-SN2-zero
-                'aeSN3':285,        # VpcbAE-SN3-zero
-                'SN1sensi':0.423,   # "Sensitivity (mV/ppb)"
-                'SN2sensiO3':0.290, # "Sensitivity (mV/ppb)"
-                'SN2sensiNO2':0.407,# "Sensitivity (mV/ppb)"
-                'SN3sensi':0.367    # "Sensitivity (mV/ppb)" 
+                'we1z':300,        # VpcbWE-SN1-zero 
+                'we2z':409,        # VpcbWE-SN2-zero
+                'we3z':281,        # VpcbWE-SN3-zero
+                'ae1z':303,        # VpcbAE-SN1-zero
+                'ae2z':419,        # VpcbAE-SN2-zero
+                'ae3z':285,        # VpcbAE-SN3-zero
+                '1sensi':0.423,    # "Sensitivity (mV/ppb)"
+                '2sensiO3':0.290,  # "Sensitivity (mV/ppb)"
+                '2sensiNO2':0.407, # "Sensitivity (mV/ppb)"
+                '3sensi':0.367     # "Sensitivity (mV/ppb)" 
             },
             'methane':False
         },
         '0000000008e366f4':{
             'name':'kit 3',
             'alphasense':{
-                'weSN1':320,        # VpcbWE-SN1-zero 
-                'weSN2':419,        # VpcbWE-SN2-zero
-                'weSN3':269,        # VpcbWE-SN3-zero
-                'aeSN1':317,        # VpcbAE-SN1-zero
-                'aeSN2':415,        # VpcbAE-SN2-zero
-                'aeSN3':267,        # VpcbAE-SN3-zero
-                'SN1sensi':0.479,   # "Sensitivity (mV/ppb)"
-                'SN2sensi03':0.293, # "Sensitivity (mV/ppb)"
-                'SN2sensiNO2':0.382,
-                'SN3sensi':0.371    # "Sensitivity (mV/ppb)" 
+                'we1z':320,        # VpcbWE-SN1-zero 
+                'we2z':419,        # VpcbWE-SN2-zero
+                'we3z':269,        # VpcbWE-SN3-zero
+                'ae1z':317,        # VpcbAE-SN1-zero
+                'ae2z':415,        # VpcbAE-SN2-zero
+                'ae3z':267,        # VpcbAE-SN3-zero
+                '1sensi':0.479,    # "Sensitivity (mV/ppb)"
+                '2sensi03':0.293,  # "Sensitivity (mV/ppb)"
+                '2sensiNO2':0.382,
+                '3sensi':0.371     # "Sensitivity (mV/ppb)" 
             },
             'methane':False
         },
         '00000000b6e2f676':{
             'name':'kit 4',
             'alphasense':{
-                'weSN1':299,        # VpcbWE-SN1-zero 
-                'weSN2':409,        # VpcbWE-SN2-zero
-                'weSN3':267,        # VpcbWE-SN3-zero
-                'aeSN1':309,        # VpcbAE-SN1-zero
-                'aeSN2':419,        # VpcbAE-SN2-zero
-                'aeSN3':284,        # VpcbAE-SN3-zero
-                'SN1sensi':0.376,   # "Sensitivity (mV/ppb)"
-                'SN2sensiO3':0.404, # "Sensitivity (mV/ppb)"
-                'SN2sensiNO2':0.416
-                'SN3sensi':0.352    # "Sensitivity (mV/ppb)" 
+                'we1z':299,        # VpcbWE-SN1-zero 
+                'we2z':409,        # VpcbWE-SN2-zero
+                'we3z':267,        # VpcbWE-SN3-zero
+                'ae1z':309,        # VpcbAE-SN1-zero
+                'ae2z':419,        # VpcbAE-SN2-zero
+                'ae3z':284,        # VpcbAE-SN3-zero
+                '1sensi':0.376,    # "Sensitivity (mV/ppb)"
+                '2sensiO3':0.404,  # "Sensitivity (mV/ppb)"
+                '2sensiNO2':0.416
+                '3sensi':0.352     # "Sensitivity (mV/ppb)" 
             },
             'methane':False
         },
         '00000000cb78dc37':{
             'name':'kit 5',
             'alphasense':{
-                'weSN1':305,        # VpcbWE-SN1-zero 
-                'weSN2':351,        # VpcbWE-SN2-zero
-                'weSN3':266,        # VpcbWE-SN3-zero
-                'aeSN1':313,        # VpcbAE-SN1-zero
-                'aeSN2':435,        # VpcbAE-SN2-zero
-                'aeSN3':284,        # VpcbAE-SN3-zero
-                'SN1sensi':0.314,   # "Sensitivity (mV/ppb)"
-                'SN2sensiO3':0.351, # "Sensitivity (mV/ppb)"
-                'SN2sensiNO2':0.420,
-                'SN3sensi':0.380    # "Sensitivity (mV/ppb)" 
+                'we1z':305,        # VpcbWE-SN1-zero 
+                'we2z':351,        # VpcbWE-SN2-zero
+                'we3z':266,        # VpcbWE-SN3-zero
+                'ae1z':313,        # VpcbAE-SN1-zero
+                'ae2z':435,        # VpcbAE-SN2-zero
+                'ae3z':284,        # VpcbAE-SN3-zero
+                '1sensi':0.314,    # "Sensitivity (mV/ppb)"
+                '2sensiO3':0.351,  # "Sensitivity (mV/ppb)"
+                '2sensiNO2':0.420,
+                '3sensi':0.380     # "Sensitivity (mV/ppb)" 
             },
             'methane':False
         },
         '000000003068b18f':{
             'name':'kit 6',
             'alphasense':{
-                'weSN1':0,        # VpcbWE-SN1-zero 
-                'weSN2':0,        # VpcbWE-SN2-zero
-                'weSN3':0,        # VpcbWE-SN3-zero
-                'aeSN1':0,        # VpcbAE-SN1-zero
-                'aeSN2':0,        # VpcbAE-SN2-zero
-                'aeSN3':0,        # VpcbAE-SN3-zero
-                'SN1sensi':0.,    # "Sensitivity (mV/ppb)"
-                'SN2sensi':0.,    # "Sensitivity (mV/ppb)"
-                'SN3sensi':0.     # "Sensitivity (mV/ppb)" 
+                'we1z':0,        # VpcbWE-SN1-zero 
+                'we2z':0,        # VpcbWE-SN2-zero
+                'we3z':0,        # VpcbWE-SN3-zero
+                'ae1z':0,        # VpcbAE-SN1-zero
+                'ae2z':0,        # VpcbAE-SN2-zero
+                'ae3z':0,        # VpcbAE-SN3-zero
+                '1sensi':0.0,     # "Sensitivity (mV/ppb)"
+                '2sensi':0.0,     # "Sensitivity (mV/ppb)"
+                '3sensi':0.0      # "Sensitivity (mV/ppb)" 
             },
             'methane':False
         }
