8514eb17c0eeab812be5884cca280b412a4810f3
Tom Keene <tom@theanthillsocial.co.uk>
2014-09-04 10:14:07 +0100

    Deleted simple server and made some name changes

--- startup_script.sh
+++ startup_script.sh
@@ -1,21 +1,13 @@
 #!/bin/sh 
+# Script is called from systemd on boot
 
-# Set the font & keyboartd
+# Set the font & keyboard: TODO: Possible make perminent 
 loadkeys uk
 setfont ter-p24b.psf.gz
 
-# Make USB bus power ediable: TODO: Doesn't need to be 777 just testing...
-chmod 777 /sys/devices/platform/bcm2708_usb/bussuspend
-chmod 777 /sys/devices/platform/bcm2708_usb/buspower
-
-# Start an ultra minimal local server	
-#killall python
-#cd /home/csk/sensorcoms/public	 
-#python -m http.server 80 &
-
 # Run the sensor app 
 #/home/csk/sensorcoms/app.py > /dev/null 2>&1
-/home/csk/sensorcoms/app.py &
+/home/csk/csk/app.py &
 
 # Startup GPSD
 killall gpsd
@@ -24,5 +16,5 @@ gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock &
 
 # Check if we got to the end of the script
 MYDATE=date
-echo $MYDATE >> /home/csk/log.txt	 
+echo "$MYDATE: Started GPSD and app.py application" >> /home/csk/csk.log 
 
