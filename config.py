ba11b2c3a881e99372a3e4c75406741615861e3b
Tom Keene <tom@theanthillsocial.co.uk>
2014-09-30 15:41:26 +0100

    Added config

---
+++ config.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python3
+import json,subprocess
+
+# Construct the config for this sspecific device
+def init():
+
+    # Config values
+    CONFIG = {
+        'posturl':'http://frackbox.citizensense.net/api',
+        '0000000080c169f1':{
+            'name':'kit 1',
+            'alphasense':{
+                'weSN1':310,        # VpcbWE-SN1-zero 
+                'weSN2':412,        # VpcbWE-SN2-zero
+                'weSN3':280,        # VpcbWE-SN3-zero
+                'aeSN1':312,        # VpcbAE-SN1-zero
+                'aeSN2':413,        # VpcbAE-SN2-zero
+                'aeSN3':270,        # VpcbAE-SN3-zero
+                'SN1sensi':0.54,    # "Sensitivity (mV/ppb)"
+                'SN2sensi':0.238,   # "Sensitivity (mV/ppb)"
+                'SN3sensi':0.362    # "Sensitivity (mV/ppb)" 
+            },
+            'methane':False
+        },
+        '0':{
+            'name':'kit 2',
+            'alphasense':{
+                'weSN1':310,        # VpcbWE-SN1-zero 
+                'weSN2':412,        # VpcbWE-SN2-zero
+                'weSN3':280,        # VpcbWE-SN3-zero
+                'aeSN1':312,        # VpcbAE-SN1-zero
+                'aeSN2':413,        # VpcbAE-SN2-zero
+                'aeSN3':270,        # VpcbAE-SN3-zero
+                'SN1sensi':0.54,    # "Sensitivity (mV/ppb)"
+                'SN2sensi':0.238,   # "Sensitivity (mV/ppb)"
+                'SN3sensi':0.362    # "Sensitivity (mV/ppb)" 
+            },
+            'methane':False
+        },
+        '0':{
+            'name':'kit 3',
+            'alphasense':{
+                'weSN1':310,        # VpcbWE-SN1-zero 
+                'weSN2':412,        # VpcbWE-SN2-zero
+                'weSN3':280,        # VpcbWE-SN3-zero
+                'aeSN1':312,        # VpcbAE-SN1-zero
+                'aeSN2':413,        # VpcbAE-SN2-zero
+                'aeSN3':270,        # VpcbAE-SN3-zero
+                'SN1sensi':0.54,    # "Sensitivity (mV/ppb)"
+                'SN2sensi':0.238,   # "Sensitivity (mV/ppb)"
+                'SN3sensi':0.362    # "Sensitivity (mV/ppb)" 
+            },
+            'methane':False
+        },
+        '0':{
+            'name':'kit 4',
+            'alphasense':{
+                'weSN1':310,        # VpcbWE-SN1-zero 
+                'weSN2':412,        # VpcbWE-SN2-zero
+                'weSN3':280,        # VpcbWE-SN3-zero
+                'aeSN1':312,        # VpcbAE-SN1-zero
+                'aeSN2':413,        # VpcbAE-SN2-zero
+                'aeSN3':270,        # VpcbAE-SN3-zero
+                'SN1sensi':0.54,    # "Sensitivity (mV/ppb)"
+                'SN2sensi':0.238,   # "Sensitivity (mV/ppb)"
+                'SN3sensi':0.362    # "Sensitivity (mV/ppb)" 
+            },
+            'methane':False
+        },
+        '00000000cb78dc37':{
+            'name':'kit 5',
+            'alphasense':{
+                'weSN1':310,        # VpcbWE-SN1-zero 
+                'weSN2':412,        # VpcbWE-SN2-zero
+                'weSN3':280,        # VpcbWE-SN3-zero
+                'aeSN1':312,        # VpcbAE-SN1-zero
+                'aeSN2':413,        # VpcbAE-SN2-zero
+                'aeSN3':270,        # VpcbAE-SN3-zero
+                'SN1sensi':0.54,    # "Sensitivity (mV/ppb)"
+                'SN2sensi':0.238,   # "Sensitivity (mV/ppb)"
+                'SN3sensi':0.362    # "Sensitivity (mV/ppb)" 
+            },
+            'methane':False
+        },
+        '0':{
+            'name':'kit 6',
+            'alphasense':{
+                'weSN1':310,        # VpcbWE-SN1-zero 
+                'weSN2':412,        # VpcbWE-SN2-zero
+                'weSN3':280,        # VpcbWE-SN3-zero
+                'aeSN1':312,        # VpcbAE-SN1-zero
+                'aeSN2':413,        # VpcbAE-SN2-zero
+                'aeSN3':270,        # VpcbAE-SN3-zero
+                'SN1sensi':0.54,    # "Sensitivity (mV/ppb)"
+                'SN2sensi':0.238,   # "Sensitivity (mV/ppb)"
+                'SN3sensi':0.362    # "Sensitivity (mV/ppb)" 
+            },
+            'methane':False
+        }
+    }
+
+    # Load the config for this specific device
+    jsonstr = subprocess.check_output("libraries/RPiInfo.sh", shell=True).decode("utf-8")
+    try:
+        info=json.loads(jsonstr)
+        serial = info["serial"] 
+        CONFIG[serial]['serial'] = serial
+        CONFIG[serial]['posturl'] = CONFIG['posturl']
+        return CONFIG[serial] 
+    except ValueError:
+        return False
+
+if __name__=='__main__':
+    print(init())
+
