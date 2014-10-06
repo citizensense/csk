6abaafb40cff8439cc17b3101b8eb0734355787c
Tom Keene <tom@theanthillsocial.co.uk>
2014-10-06 16:26:14 +0100

    Simplified wind direction module

--- libraries/winddir.py
+++ libraries/winddir.py
@@ -4,10 +4,24 @@ import json
 
 # Small class to convert a resistance value to a compass point
 class winddir:
-
+    
+    # Lets startup
     def __init__(self):
         # Amount to brack ether side of the 'sweet spot'
         self.bracket = 10
+    
+    # Now grab some data
+    def grabdir(self):
+         # First grab a value from the adc
+        try:
+            jsonstr = subprocess.check_output("/home/csk/csk/libraries/MCP3008.py", shell=True).decode("utf-8")
+            adc=json.loads(jsonstr)
+            val = adc['5']
+        except Exception as e:
+            return False
+        # Now convert it to a compass reading
+        point = self.compass(val)
+        return point
 
     def compass(self, res):
         if self.isrange(781, res): return 'N'
@@ -25,21 +39,11 @@ class winddir:
         if val <= mmax and val >= mmin: return True
         else: return False
 
+    
 if __name__=='__main__':
     import subprocess, time
     wind = winddir()
     # Read the wind direction
     while True:
-        # First grab a value from the adc
-        try:
-            jsonstr = subprocess.check_output("./MCP3008.py", shell=True).decode("utf-8")
-            adc=json.loads(jsonstr)
-            val = adc['5']
-        except Exception as e:
-            print('No ADC: '+str(e))
-            val = False
-        # Now convert it to a compass reading
-        if val:
-            point = wind.compass(val)
-            print(str(val)+': '+point)
-        time.sleep(1)
+        print(wind.grabdir())
+        time.sleep(5)
